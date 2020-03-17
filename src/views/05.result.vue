ccde16a50c10248de9a331211fe15d5fd0057195
xrzi2016 <xrzi2016.aliyun.com>
2020-03-17 20:11:16 +0800

    first

---
+++ src/views/05.result.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="result-container">
+    <div class="title-wrap">
+      <h2 class="title" :keywords="(keywords = $route.query.q)">{{ $route.query.q }}</h2>
+      <span class="sub-title">{{ count }}</span>
+    </div>
+    <el-tabs v-model="activeIndex">
+      <el-tab-pane label="歌曲" name="songs">
+        <table class="el-table">
+          <thead>
+            <th></th>
+            <th>音乐标题</th>
+            <th>歌手</th>
+            <th>专辑</th>
+            <th>时长</th>
+          </thead>
+          <tbody>
+            <tr class="el-table__row" v-for="(item, index) in songs" :key="index" @click="playMusic(item.id)">
+              <td>{{ index + 1 }}</td>
+              <td>
+                <div class="song-wrap">
+                  <div class="name-wrap">
+                    <!-- 名称 -->
+                    <span>{{ item.name }}</span>
+                    <!-- mv图标 -->
+                    <span v-if="item.mvid !== 0" class="iconfont icon-mv"></span>
+                  </div>
+                  <!-- 二级标题 -->
+                  <span v-if="item.alias.length !== 0">{{ item.alias[0] }}</span>
+                </div>
+              </td>
+              <td>{{ item.artists[0].name }}</td>
+              <td>{{ item.album.name }}</td>
+              <td>{{ item.duration | playTimeFormat }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </el-tab-pane>
+
+      <el-tab-pane label="歌单" name="lists">
+        <div class="items">
+          <div class="item" v-for="(item, index) in playlists" :key="index" @click="toPlaylist(item.id)">
+            <div class="img-wrap">
+              <div class="num-wrap">
+                播放量:
+                <span class="num">{{ item.playCount | playNumFormat }}</span>
+              </div>
+              <img :src="item.coverImgUrl" alt="" />
+              <span class="iconfont icon-play"></span>
+            </div>
+            <p class="name">{{ item.name }}</p>
+          </div>
+        </div>
+      </el-tab-pane>
+
+      <el-tab-pane label="MV" name="mv">
+        <div class="items mv">
+          <div class="item" v-for="(item, index) in mvs" :key="index" @click="toMv(item.id)">
+            <div class="img-wrap">
+              <img :src="item.cover" alt="" />
+              <span class="iconfont icon-play"></span>
+              <div class="num-wrap">
+                <div class="iconfont icon-play"></div>
+                <div class="num">{{ item.playCount | playNumFormat }}</div>
+              </div>
+              <span class="time">{{ item.duration | playTimeFormat }}</span>
+            </div>
+            <div class="info-wrap">
+              <div class="name">{{ item.name }}</div>
+              <div class="singer">{{ item.artistName }}</div>
+            </div>
+          </div>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import { getMusicUrl, search } from 'network/request'
+
+export default {
+  name: 'result',
+  data() {
+    return {
+      keywords: '',
+      activeIndex: 'songs',
+      songs: [],
+      count: 0,
+      playlists: [],
+      mvs: [],
+      songId: 0,
+      type: 1
+    }
+  },
+
+  created() {
+    this._search()
+  },
+
+  watch: {
+    keywords() {
+      this._search()
+    },
+
+    activeIndex() {
+      switch (this.activeIndex) {
+        case 'songs':
+          this.type = 1
+          break
+        case 'lists':
+          this.type = 1000
+          break
+        case 'mv':
+          this.type = 1004
+          break
+
+        default:
+          break
+      }
+      this._search()
+    }
+  },
+
+  methods: {
+    async _search() {
+      const { data: resp } = await search({
+        s: this.keywords,
+        type: this.type,
+        limit: this.type === 1004 ? 8 : 10,
+        offset: 0
+      })
+      if (this.type === 1) {
+        this.songs = resp.result.songs
+        this.count = resp.result.songCount
+      } else if (this.type === 1000) {
+        this.playlists = resp.result.playlists
+        this.count = resp.result.playlistCount
+      } else if (this.type === 1004) {
+        this.mvs = resp.result.mvs
+        this.count = resp.result.mvCount
+      }
+    },
+
+    async playMusic(id) {
+      // 设置给父组件的播放地址
+      // this.$parent.musicUrl = `http://music.163.com/song/media/outer/url?id=${id}.mp3`
+
+      // 获取歌曲播放地址
+      const { data: resp } = await getMusicUrl(id)
+      if (!resp.data[0].url) {
+        return this.$message.error('该资源为VIP专享，暂不支持播放 ！')
+      }
+      // 设置给父组件的播放地址
+      this.$parent.musicUrl = resp.data[0].url
+    },
+
+    toPlaylist(id) {
+      this.$router.push(`/playlist?id=${id}`)
+    },
+
+    toMv(id) {
+      this.$router.push(`/mv?id=${id}`)
+    }
+  }
+}
+</script>
+
+<style >
+</style>
